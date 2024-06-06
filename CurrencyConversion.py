4af854646594111f6b6e085cc578a91ccbf601d1
BogomilaK <you@example.com>
2024-06-06 14:05:52 +0300

    successfully saving to conversions.json, caching func in process
    Signed-off-by: BogomilaK <you@example.com>

--- CurrencyConversion.py
+++ CurrencyConversion.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 import requests
 import supported_currencies
@@ -62,21 +63,51 @@ def request_data_for_conversion():
     date = None
     while date is None:
         #TODO: check if it is correct to add the labels (maybe del)
-        #TODO: add validator - date before today
-        #TODO: ensure correct type of data used
+        #TODO: add validator - date before today and not too much in the past(see how much is allowed)
         date = input('python3 CurrencyConversion.py ')
         check_if_command_is_end(date)
+        # try:
+        #     input_datetime_format =  datetime.datetime.strptime(date, '%Y-%m-%d')
+        #     today_datetime_format = datetime.datetime.strptime(datetime.datetime.strftime(datetime.datetime.today(), '%Y-%m-%d'))
+        #     today_datetime_format > input_datetime_format
+        # except:
+        #     print('elelel')
+        # try:
+        #     input_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        #     today_date = datetime.datetime.strptime(str(datetime.date.today), '%Y-%m-%d')
+        #     today_date > input_date
+        # except:
+        #     date = None
+        #     print('Please enter a date in the past')
+        # try:
+        #     today = datetime.datetime.strptime(str(datetime.datetime.now()), '%Y-%m-%d')
+        #     print(today)
+        #     datetime.datetime.strptime(date, '%Y-%m-%d') < datetime.datetime.strptime(str(datetime.datetime.now()),
+        #                                                                               '%Y-%m-%d')
+        #     # datetime.datetime(date, '%Y-%m-%d') < datetime.datetime(datetime.datetime.now(), '%Y-%m-%d')
+        # except:
+        #     date = None
+        #     print('Please enter a date in the past')
         try:
             datetime.date.fromisoformat(date)
         except:
             date = None
             print('Please enter a valid data format: YYYY-MM-DD')
 
+
     amount = None
     while amount is None:
-        amount = float(input('//Amount\n'))
+        # amount = float(input('//Amount\n'))
+        amount = int(input('//Amount\n'))
+        # if type(amount) not in (int, float):
+        #     print('Amount should be a value')
+        #     amount = None
+        # if not type(amount) == int or type(amount) == float:
+        #     print('Amount should be a value')
+        #     amount = None
         check_if_command_is_end(amount)
         print(len(str(amount).rsplit('.')[-1]))
+        # if len(str(amount).rsplit('.')[-1]) > 2:
         if len(str(amount).rsplit('.')[-1]) > 2:
             print('Please enter a valid amount')
             amount = None
@@ -98,55 +129,55 @@ def request_data_for_conversion():
 
 
 def save_to_conversions(date, amount, base_curr, target_curr, converted_amount):
-    #TODO: save to conversions.json
-    '''
-    with open(r'conversions.json', 'a') as f:
-        # The previous line will create the json file if doesn't exist
-        Thing = {}
-        Thing['stuff'] = []
-        Thing['stuff'].append({
-            "date": date,
-            "amount": amount,
-            "base_currency": base_curr,
-            "target_currency": target_curr,
-            "converted_amount": converted_amount
-        })
-        json.dump(Thing, f)
-        '''
-
-    conversions = {
+    entry = {
         "date": date,
         "amount": amount,
         "base_currency": base_curr,
         "target_currency": target_curr,
         "converted_amount": converted_amount
     }
-    # #TODO: dict to json
 
-    with open("conversions.json", "a") as outfile:
-        json.dump(conversions, outfile)
+    a = []
+    if not os.path.isfile('conversions.json'):
+        a.append(entry)
+        with open('conversions.json', mode='w') as f:
+            f.write(json.dumps(a, indent=2))
+    else:
+        with open('conversions.json') as feedsjson:
+            feeds = json.load(feedsjson)
 
-    '''
-    Below not working:
-    conversions = {
-        "date": date,
-        "amount": amount,
-        "base_currency": base_curr,
-        "target_currency": target_curr,
-        "converted_amount": converted_amount
-    }
-    with open('conversions.json', 'r') as file:
-        # read JSON data
-        data = json.load(file)
+        feeds.append(entry)
+        with open('conversions.json', mode='w') as f:
+            f.write(json.dumps(feeds, indent=2))
 
-        # add field
-        data["demo"]["f'{datetime.datetime.now}'"] = conversions
-'''
 
+# def compare_values(small_val, large_val):
+#     if isinstance(small_val, dict) and isinstance(large_val, dict):
+#         return all(compare_values(small_val[k], large_val.get(k)) for k in small_val)
+#     elif isinstance(small_val,list) and isinstance(large_val, list):
+#         return all(any(compare_values(small_elem, large_elem) for large_elem in large_val) for small_elem in small_val)
+#     else:
+#         return small_val == large_val
 
 
 if __name__ == '__main__':
     date, amount, base_curr, target_curr = request_data_for_conversion()
+    # #TODO: if in conversions
+    # curr = {
+    # "date": date,
+    # "amount": amount,
+    # "base_currency": base_curr,
+    # "target_currency": target_curr,
+    # }
+    # with open('conversions.json') as feedsjson:
+    #     feeds = json.load(feedsjson)
+    #     for key in ('date', 'amount', 'base_currency', 'target_currency'):
+    #         if key in curr:
+    #             if key not in feeds or not compare_values(curr[key], feeds[key]):
+    #                 print(True)
+        # print(f'{amount:.2f} {base_curr.upper()} is {feeds["converted_amount"]} {target_curr.upper()}')
+        # print(False)
+        # return True
     exchange_rate = find_exchange_rate(date, base_curr, target_curr)
     converted_amount = calculate_currency(amount, exchange_rate)
     save_to_conversions(date=date, amount=f'{amount:.2f}', base_curr=base_curr.upper(), target_curr=target_curr.upper(), converted_amount=f'{converted_amount:.2f}')
